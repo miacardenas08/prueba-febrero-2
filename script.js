e7d05f1059585251dc8935f50430913fbc7573b8
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2024-02-26 10:55:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let resultado 
+let monto = Number(prompt('Por favor, ingrese un numero'))
+
+ if (monto >= 10000) {  
+   monto = (10 * monto) /100 
+   
+ } else if (monto >= 30000) {
+
+ } else if (monto >= 100000){
+
+ }
+
+ 
